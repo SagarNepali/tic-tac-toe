57d6801e8119748ac3728ace76c7e3dd8b633e85
Sagar <worthless.dumboo@gmail.com>
2022-01-05 15:33:15 -0600

    Tic-tac-toe app based on React from its own website

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from '../src/components/game/game.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
